42cbc2a68c18cc71e608fbcd66b6cb6fdaa32934
xiejihuang@baidu.com <xiejihuang@baidu.com>
2024-10-22 00:32:03 +0800

    fix: 去除env

--- src/db/mysql/index.ts
+++ src/db/mysql/index.ts
@@ -1,18 +1,13 @@
 import { getConfig } from 'alemonjs'
 import { Sequelize } from 'sequelize'
 const config = getConfig()
-const MDB = config.value.db
-export const sequelize = new Sequelize(
-  process.env?.APP_MYSQL_DATABASE ?? MDB?.database,
-  process.env?.APP_MYSQL_USER ?? MDB?.user,
-  process.env?.APP_MYSQL_PASSWORD ?? MDB?.password,
-  {
-    host: process.env?.APP_MYSQL_HOST ?? MDB?.host,
-    port: Number(process.env?.APP_MYSQL_PROT ?? MDB?.port),
-    dialect: 'mysql',
-    logging: false // 禁用日志记录
-  }
-)
+const db = config.value.db
+export const sequelize = new Sequelize(db?.database, db?.user, db?.password, {
+  host: db?.host,
+  port: Number(db?.port),
+  dialect: 'mysql',
+  logging: false // 禁用日志记录
+})
 export const TableConfig = {
   freezeTableName: true, //不增加复数表名
   createdAt: false, //去掉
